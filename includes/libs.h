1fa87efcb6c3173e8135ad5d029a68aa0e669adf
Alexandre Neuwald <aneuwald@gmail.com>
2021-09-21 02:03:58 +0100

    last update

--- includes/libs.h
+++ includes/libs.h
@@ -6,7 +6,7 @@
 /*   By: aneuwald <aneuwald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/20 23:52:09 by aneuwald          #+#    #+#             */
-/*   Updated: 2021/09/20 23:59:02 by aneuwald         ###   ########.fr       */
+/*   Updated: 2021/09/21 02:03:31 by aneuwald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <stdlib.h>
 # include <stdio.h>
 # include <math.h>
+/*# include "mlx.h"*/
 
 # include "keymap.h"
 # include "structs.h"
 # include "functions.h"
 # include "../minilibx-linux/mlx.h"
-/*# include "mlx.h"*/
 
 
 #endif
